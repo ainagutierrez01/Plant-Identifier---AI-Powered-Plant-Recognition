9476f54dc172fb2478b8e928fce3fc505dff5011
Aina Gutierrez <aina04g@gmail.com>
2025-03-29 12:27:34 +0100

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  env: {
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY
+  },
+}
+
+export default nextConfig
